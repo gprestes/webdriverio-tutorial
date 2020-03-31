3b1a4e4bb6a655e2bafb13cc10a2fe6dc200712b
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2020-03-31 15:56:05 +0200

    Add tests from chapter 2.1

---
+++ test/element.test.js
@@ -0,0 +1,17 @@
+describe("Interacting with elements", function () {
+    it("Get text for element", () => {
+        browser.url('/')
+        
+        let text = $("h2").getText()
+        console.log(text)
+
+        text = $(".heading").getText()
+        console.log(text)
+
+        text = $("#page-footer").getText()
+        console.log(text)
+        
+        text = $("//*[@id='page-footer']").getText()
+        console.log(text)
+    })
+})
