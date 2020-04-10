85dddf9cd09e738619a9c61c46f4d6f38e573492
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2020-04-10 13:22:18 +0200

    Add scrollIntoView in chapter 3.3

---
+++ test/scrollTo.test.js
@@ -0,0 +1,16 @@
+const internetPage = require('../pages/internet.page')
+
+describe('Scroll to Element', function () {
+    it.skip('should scroll to the footer', () =>{
+        browser.url('/')
+        internetPage.pageHeader.waitForDisplayed()
+        internetPage.scrollToPageFooter()
+        assert.equal(true, internetPage.pageFooter.isDisplayedInViewport())
+    })
+
+    it('should scroll into view', () => {
+        browser.url('/')
+        internetPage.pageFooter.scrollIntoView()
+        assert.equal(true, internetPage.pageFooter.isDisplayedInViewport())
+    })
+})
